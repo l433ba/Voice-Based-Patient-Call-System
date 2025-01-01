3885fa62a3ea1f81155ea465855feaea311730a4
kailash Maurya <kailashmaurya51@gmail.com>
2025-01-02 04:01:56 +0530

    Remove sensitive files and node_modules from server directory

--- Server/index.js
+++
@@ -1,2 +0,0 @@
-
-console.log(`Hello Node.js v${process.versions.node}!`);
--- Server/src/interfaces/IRequest.ts
+++
@@ -1,5 +0,0 @@
-interface IRequest {
-    // ... existing properties ...
-    notes?: string;
-    completionTime?: Date;
-} 
